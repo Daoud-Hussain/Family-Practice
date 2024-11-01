19eff43dbab696f50249621110e372a5234509ee
Daoud-Hussain <daoudhussain302@gmail.com>
2024-11-01 21:10:03 +0500

    ft. UI fixes

--- components/anxiety-treatment.vue
+++ components/anxiety-treatment.vue
@@ -2,42 +2,45 @@
   <section
     ref="container"
     class="hero-section flex flex-col lg:flex-col justify-around items-center pb-10"
-  > 
-    <div class="main-container flex flex-col lg:flex-row justify-around items-center py-16 lg:pt-16">
-    
-    <div class="right-content-wrapper">
-  <div class="background-overlay"></div>
-  <img src="~/assets/anxiety.png" alt="Anxiety Image" class="anxiety-image" />
-</div>
-
-    
+  >
     <div
-      class="left-content flex flex-col items-center lg:items-start justify-center text-center lg:text-left w-full lg:w-1/2 px-6"
+      class="main-container flex flex-col lg:flex-row justify-around items-center py-16 lg:pt-16"
     >
-      
-      <h1
-        class="text-3xl pt-8 md:pt-0 lg:text-4xl font-bold leading-tight text-heroo-color mb-4"
+      <div class="right-content-wrapper">
+        <div class="background-overlay"></div>
+        <img
+          src="~/assets/anxiety.png"
+          alt="Anxiety Image"
+          class="anxiety-image"
+        />
+      </div>
+
+      <div
+        class="left-content flex flex-col items-center lg:items-start justify-center text-center lg:text-left w-full lg:w-1/2 px-6"
       >
-        Online anxiety treatment
-      </h1>
-      <p class="text-heroo-color mb-6 text-1.5xl.5">
-        Find anxiety relief with expert one-on-one medical care and anxiety
-        medication delivered each month
-      </p>
-      <p class="custom-para px-3 py-2 mb-8">$95 $25 for your first month</p>
-      <button
-        class="bg-gradient-to-r from-[#126b62] to-[#47b04b] text-white py-2 px-12 font-semibold rounded-full shadow-lg hover:from-[#47b04b] hover:to-[#126b62] transition-all"
-      >
-        Take the first Step
-      </button>
+        <h1
+          class="text-3xl pt-8 md:pt-0 lg:text-4xl font-bold leading-tight text-heroo-color mb-4"
+        >
+          Online anxiety treatment
+        </h1>
+        <p class="text-heroo-color mb-6 text-1.5xl.5">
+          Find anxiety relief with expert one-on-one medical care and anxiety
+          medication delivered each month
+        </p>
+        <p class="custom-para px-3 py-2 mb-8">$95 $25 for your first month</p>
+        <button
+          class="bg-gradient-to-r from-[#126b62] to-[#47b04b] text-white py-2 px-12 font-semibold rounded-full shadow-lg hover:from-[#47b04b] hover:to-[#126b62] transition-all"
+        >
+          Take the first Step
+        </button>
+      </div>
     </div>
 
-  </div>
-  
-  <p class="para">
-  If you're having a psychiatric emergency, call 911 or go to the nearest emergency room. 
-  You can also call the National Suicide Prevention Hotline 1-800-273-8255 or text “home” to 741-741.</p>
-  
+    <p class="para">
+      If you're having a psychiatric emergency, call 911 or go to the nearest
+      emergency room. You can also call the National Suicide Prevention Hotline
+      1-800-273-8255 or text “home” to 741-741.
+    </p>
   </section>
 </template>
 
@@ -102,10 +105,10 @@ onMounted(() => {
 .background-overlay {
   position: absolute;
   top: -40px;
-  left: -40px; /* Adjust to control how much it extends left */
+  left: -40px;
   width: 100%;
   height: 100%;
-  background-color: #ffe4e6; /* pinkish background */
+  background-color: rgb(254 202 202);
   padding: 20px;
   z-index: 0;
 }
@@ -116,18 +119,15 @@ onMounted(() => {
   max-width: 100%;
 }
 
-
-
-.custom-para{
+.custom-para {
   background-color: rgb(254 202 202);
   color: #000;
 }
 
-.para{
+.para {
   width: 60%;
   color: #000;
   text-align: center;
   font-size: large;
-
 }
 </style>
